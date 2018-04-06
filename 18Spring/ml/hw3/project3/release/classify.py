e3ba5dfc1a61120d556fb56bd123645393fd7a81
Aleck Zhao <aleck.zhao1114@gmail.com>
2018-04-06 00:32:13 -0400

    random

--- 18Spring/ml/hw3/project3/release/classify.py
+++ 18Spring/ml/hw3/project3/release/classify.py
@@ -28,7 +28,7 @@ def load_data(filename):
             
             for item in split_line[1:]:
                 try:
-                    index = int(item.split(":")[0])
+                    index = int(item.split(":")[0]) - 1
                 except ValueError:
                     raise ValueError("Unable to convert index " + item.split(":")[0] + " to integer.")
                 try:
@@ -83,34 +83,85 @@ class Adaboost(Predictor):
 
     def train(self, instances):
         N = len(instances)
+        num_features = np.max([len(instance.feature_vector._feature_vector) for instance in instances])
 
         # Initialize uniform distribution
         D = np.ones(N)/N
 
+        stop_hypothesis = self.num_boosting_iterations
+
+        def h(i, j, c, instances):
+            values = [instance.feature_vector.get(j) for instance in instances]
+            y = [instance.label._label for instance in instances]
+            x = instances[i].feature_vector._feature_vector
+
+            if (x[j] > c):
+                correct = np.where(values > c, 1, 0)
+            else:
+                correct = np.where(values <= c, 1, 0)
+
+            result = np.dot(correct, y)
+            if (result >= 0):
+                return 1
+            else:
+                return -1
+
         for t in range(self.num_boosting_iterations):
             # Initialize error array along each feature
-            error_j = np.array(N)
-            y = [instance.label for instance in instances]
+            error_j = [None] * N
+            y = np.array([instance.label._label for instance in instances])
 
-            for j in range(N):
-                values = np.array([instance.feature_vector.get(j) for instance in instances])
+            for j in range(num_features):
+                unique = np.sort(np.unique([instance.feature_vector.get(j) for instance in instances]))
 
-                # Get unique feature values in ascending order
-                unique = np.sort(np.unique(values))
+                error_c = np.zeros(len(unique))
 
-                # Initialize error array along each value cutoff
-                error_c = np.array(len(unique))
+                for k in range(len(unique)):
+                    for i in range(N):
+                        if (h(i, j, unique[k], instances) != y[i]):
+                            error_c += D[i]
+            
+                error_j[j] = error_c
 
-                for i in range(len(unique)):
-                    c = values[i]
+            # Finding index of argmin j
+            min_inds = [np.argmin(error_j[j]) for j in range(num_features)]  
+            min_feature_ind = np.argmin([error_j[j][min_inds[j]] for j in range(num_features)])
+            self.j[t] = min_feature_ind
 
-                    
+            # Finding value of argmax c
+            min_c_ind = np.argmin(error_j[min_feature_ind])
+            self.c[t] = np.sort(np.unique(np.array([instance.feature_vector.get(min_feature_ind) for instance in instances])))[min_c_ind]
 
+            error_t = 0
+            for i in range(N):
+                if(h(i, self.j[t], self.c[t], instances) != y[i]):
+                    error_t += D[i]
 
+            if (error_t < 0.000001):
+                # stop and only use up to previous hypothesis
+                stop_hypothesis = t
+                continue
 
-    def h(x, j, c, instances):
+            self.alpha[t] = 0.5 * np.log((1-error_t)/error(t))
+
+            # Calculate normalizing factor
+            Z = 0
+            for i in range(N):
+                Z += D[i] * np.exp(-self.alpha[t] * y[i] * h(i, self.j[t], self.c[t], instances))
+
+            # Calculate new distribution D
+            for i in range(N):
+                D[i] *= 1/Z * np.exp(-self.alpha[t] * y[i] * h(i, self.j[t], self.c[t], instances))
+
+        # Truncate values if we stopped early, otherwise stop hypothesis is number of iterations
+        self.j = self.j[:stop_hypothesis]
+        self.c = self.c[:stop_hypothesis]
+        self.alpha = self.alpha[:stop_hypothesis]
+
+    def h(i, j, c, instances):
         values = np.array([instance.feature_vector.get(j) for instance in instances])
-        y = np.array([instance.label.label] for instance in instances])
+        y = np.array([instance.label._label for instance in instances])
+        x = instances[i].feature_vector._feature_vector
 
         if (x[j] > c):
             correct = np.where(values > c, 1, 0)
@@ -124,10 +175,23 @@ class Adaboost(Predictor):
             return -1
 
     def predict(self, instance):
-        pass
+        # Sums for keeping track of whether y_hat = 1 or y_hat = -1 are better
+        sum_1 = 0
+        sum_2 = 0
+        for t in range(len(self.alpha)):
+            if (instance.feature_vector.get(self.j[t]) > self.c[t]):
+                sum_1 += self.alpha[t]
+            else:
+                sum_2 += self.alpha[t]
+
+        if (sum_1 >= sum_2):
+            return 1
+        else:
+            return -1
+       
 
 def train(instances, args):
-    if (args.algorithm.tolower() == 'adaboost'):
+    if (args.algorithm.lower() == 'adaboost'):
         predictor = Adaboost(args.num_boosting_iterations)
 
     predictor.train(instances)
